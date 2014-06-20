e2dce716741eba79ad04ef33a099d0082f172352
Vito <zhangwei.noair@gmail.com>
2014-06-20 14:06:21 +0800

    Enable over view mode

--- MHVideoPhotoGallery/MMHVideoPhotoGallery/MHGalleryImageViewerViewController/MHGalleryImageViewerViewController.m
+++ MHVideoPhotoGallery/MMHVideoPhotoGallery/MHGalleryImageViewerViewController/MHGalleryImageViewerViewController.m
@@ -92,19 +92,18 @@
     self.UICustomization          = self.galleryViewController.UICustomization;
     self.transitionCustomization  = self.galleryViewController.transitionCustomization;
     
-    self.navigationItem.hidesBackButton = YES;
-//    if (!self.UICustomization.showOverView) {
-//        self.navigationItem.hidesBackButton = YES;
-//    }else{
-//        if (self.galleryViewController.UICustomization.backButtonState == MHBackButtonStateWithoutBackArrow) {
-//            UIBarButtonItem *backBarButton = [UIBarButtonItem.alloc initWithImage:MHTemplateImage(@"ic_square")
-//                                                                            style:UIBarButtonItemStyleBordered
-//                                                                           target:self
-//                                                                           action:@selector(backButtonAction)];
-//            self.navigationItem.hidesBackButton = YES;
-//            self.navigationItem.leftBarButtonItem = backBarButton;
-//        }
-//    }
+    if (!self.UICustomization.showOverView) {
+        self.navigationItem.hidesBackButton = YES;
+    }else{
+        if (self.galleryViewController.UICustomization.backButtonState == MHBackButtonStateWithoutBackArrow) {
+            UIBarButtonItem *backBarButton = [UIBarButtonItem.alloc initWithImage:MHTemplateImage(@"ic_square")
+                                                                            style:UIBarButtonItemStyleBordered
+                                                                           target:self
+                                                                           action:@selector(backButtonAction)];
+            self.navigationItem.hidesBackButton = YES;
+            self.navigationItem.leftBarButtonItem = backBarButton;
+        }
+    }
     
     UIBarButtonItem *doneBarButton =  [UIBarButtonItem.alloc initWithBarButtonSystemItem:UIBarButtonSystemItemDone
                                                                                   target:self
