f7ec2efc34d17622f147f4437b42bca292505aa9
Vito <zhangwei.noair@gmail.com>
2014-08-26 12:39:57 +0800

    Refactor property name

--- MHVideoPhotoGallery/MMHVideoPhotoGallery/MHGalleryImageViewerViewController/MHGalleryImageViewerViewController.m
+++ MHVideoPhotoGallery/MMHVideoPhotoGallery/MHGalleryImageViewerViewController/MHGalleryImageViewerViewController.m
@@ -180,7 +180,7 @@
     self.descriptionView = [UITextView.alloc initWithFrame:CGRectZero];
     self.descriptionView.backgroundColor = [UIColor clearColor];
     self.descriptionView.font = [UIFont systemFontOfSize:15];
-    self.descriptionView.text = item.description;
+    self.descriptionView.text = item.itemDescription;
     self.descriptionView.textColor = [UIColor blackColor];
     self.descriptionView.scrollEnabled = NO;
     self.descriptionView.userInteractionEnabled = NO;
@@ -292,7 +292,7 @@
 -(void)updateDescriptionLabelForIndex:(NSInteger)index{
     if (index < self.numberOfGalleryItems) {
         MHGalleryItem *item = [self itemForIndex:index];
-        self.descriptionView.text = item.description;
+        self.descriptionView.text = item.itemDescription;
         
         if (item.attributedString) {
             self.descriptionView.attributedText = item.attributedString;
